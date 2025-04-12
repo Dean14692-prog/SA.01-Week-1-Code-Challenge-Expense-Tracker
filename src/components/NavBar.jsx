5699d8b1955e912d2a9c95ea902f1f946f080257
Dean14692-prog <dennismuasyangui@gmail.com>
2025-04-12 18:11:08 +0300

    search

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,7 @@
 export default function Navbar() {
   return (
     <div
-      className="d-flex justify-content-between align-items-center mb-1 px-3 py-2 rounded-3
-          shadow-sm border "
+      className="d-flex justify-content-between align-items-center px-3 py-2 rounded-3 shadow-sm border w-90 mb-1"
       style={{ backgroundColor: "#0d6efd", height: "80px" }}
     >
       <nav className="navbar" data-bs-theme="light">
